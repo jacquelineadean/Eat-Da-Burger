280d9625a5b09363025f43b66950c8b7e9acd654
Jacqueline Dean <jacquelineadean@gmail.com>
2020-07-23 15:37:12 -0500

    Setup Database

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drop any previously existing databases called burgers_db and create the new database
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create table called Burgers
+CREATE TABLE burgers (
+    -- Create columns within table
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+
+    -- Set id as the primary key
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("ShackBurger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("'Shroom Burger", FALSE);
