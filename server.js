341a6c6e2f30c7a93402a8c589119da50966ac8b
Jacqueline Dean <jacquelineadean@gmail.com>
2020-07-23 15:18:53 -0500

    Initializing files

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies 
+// ==========================================================
+
+const express = require("express");
+
+// Sets up the Express App
+// ==========================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
